3d17c03d0803fc2d76abda74bb2b88040a805279
rahul <rahuloberoi71@gmail.com>
2017-04-14 17:36:17 +0530

    optimised ques6

--- ch2/ques7.c
+++ ch2/ques7.c
@@ -10,13 +10,13 @@ int main(int argc, char const *argv[])
 	if(ch>=65&&ch<=90)
 		printf("\nThe Character is a uppercase latter\n");
 
-	if(ch>=97&&ch<+122)
+	else if(ch>=97&&ch<=122)
 		printf("\nCharacter is lower case\n");
 
-	if(ch>=48&&ch<=57)
+	else if(ch>=48&&ch<=57)
 		printf("character is a number\n");
 
-	if(ch>=0&&ch<=47 || ch>=58&&ch<=64 || ch>=91&&ch<=96 || ch>=123&&ch<=127)
+	else
 	    printf("character is a special symbol\n");
 
 }
